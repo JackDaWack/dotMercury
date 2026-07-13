b7ec75145b5b7595c3fc6dad206e7726fbe89984
JackDaWack <jackson.s.netter49@gmail.com>
2026-07-13 17:41:11 -0400

    Implemented message list.

--- Frontend/messagelist.jsx
+++ Frontend/messagelist.jsx
@@ -1,3 +1,21 @@
 import MessageRow from "./MessageRow";
 import SearchBox from "../common/SearchBox";
 import Toolbar from "../common/Toolbar";
+
+export default function MessageList({ messages, onSearch, onSelectMessage }) {
+  return (
+    <div className="message-list">
+      <Toolbar />
+      <SearchBox onSearch={onSearch} />
+      <div className="message-rows">
+        {messages.map((message) => (
+          <MessageRow
+            key={message.id}
+            message={message}
+            onSelect={onSelectMessage}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
