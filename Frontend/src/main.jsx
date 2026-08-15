ec1bec0bbba7894f849a956f7b962b640a2bfdd7
JackDaWack <jackson.s.netter49@gmail.com>
2026-08-15 16:57:17 -0400

    quick frontend edit.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './app.jsx'
 import UnauthenticatedHeader from './components/UnauthenticatedHeader.jsx'
 
 createRoot(document.getElementById('root')).render(
