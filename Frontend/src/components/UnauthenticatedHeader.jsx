03b782a54b47e83bfd1d105049f5ab7ef9b662a5
JackDaWack <jackson.s.netter49@gmail.com>
2026-08-15 13:26:01 -0400

    Added and implemented unauthenticated header component.

---
+++ Frontend/src/components/UnauthenticatedHeader.jsx
@@ -0,0 +1,14 @@
+function UnauthenticatedHeader() {
+  return (
+    <header>
+      <h1>dotMercury</h1>
+      <nav>
+        <ul>
+          <li><a href="/login">Login</a></li>
+          <li><a href="/register">Register</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+export default UnauthenticatedHeader;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import UnauthenticatedHeader from './components/UnauthenticatedHeader.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <UnauthenticatedHeader />
     <App />
   </StrictMode>,
 )
