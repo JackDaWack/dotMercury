0806ec090c7946174c9ef9378ba12e732d55d5d2
JackDaWack <jackson.s.netter49@gmail.com>
2026-08-18 18:31:46 -0400

    Added export to login component. Implemented registry component.

--- Frontend/src/register.jsx
+++ Frontend/src/register.jsx
@@ -1,21 +1,23 @@
-import { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function Login() {
+function Register() {
     const [message, setMessage] = useState("");
     useEffect(() => {
-        fetch("http://localhost:8000/api/login")
+        fetch("http://localhost:8000/api/register")
             .then(response => response.json())
             .then(data => setMessage(data.message));
     }, []);
     return (
         <div>     
-            <h1>Login to dotMercury</h1>
+            <h1>Register for dotMercury</h1>
             <label htmlFor="username">Username:</label>
             <input type="text" id="username" name="username" />
+            <label htmlFor="email">Email:</label>
+            <input type="email" id="email" name="email" />
             <label htmlFor="password">Password:</label>
             <input type="password" id="password" name="password" />
         </div>
     );
 }
 
-export default Login;
+export default Register;
