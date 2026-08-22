3fca097428adea746fe7e47111d68ccebd1bc243
JackDaWack <jackson.s.netter49@gmail.com>
2026-08-22 10:03:44 -0400

    Implemented footer.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './app.jsx'
 import UnauthenticatedHeader from './components/UnauthenticatedHeader.jsx'
+import Footer from './components/Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UnauthenticatedHeader />
     <App />
+    <Footer />
   </StrictMode>,
 )
