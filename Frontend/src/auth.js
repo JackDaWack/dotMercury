4f2ccdc096a0e7f0aa640ba933a1300c40a1563a
JackDaWack <jackson.s.netter49@gmail.com>
2026-09-08 18:00:54 -0400

    Created new auth.js file.

---
+++ Frontend/src/auth.js
@@ -0,0 +1,18 @@
+async function login() {
+    try {
+        const email = document.querySelector("#login-form #email").value;
+        const password = document.querySelector("#login-form #password").value;
+        const response = await fetch("/login", {method: "POST", headers: {"Content-Type": "application/json"}, body: JSON.stringify({username,password})})
+        if (!response.ok) {
+            throw new Error("User login request failed");
+        }
+        const data = await response.json();
+        console.log("Server response:", data);
+        if (data.success) {
+            window.location.href = "/";
+        } else {
+            alert("Login failed");
+        }
+    }
+    catch(err){console.error("Error calling backend:", err);}
+}
