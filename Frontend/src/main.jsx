73604b8738049519163c7a8af0fd7147bd19ef63
JackDaWack <jackson.s.netter49@gmail.com>
2026-09-14 19:08:20 -0400

    Continuing implementation of app navigation.

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -4,16 +4,27 @@ import './index.css'
 import App from './app.jsx'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import Settings from './components/Settings.jsx'
 import { useState } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
 
 function Root() {
   const [authView, setAuthView] = useState('login')
-
   return (
     <StrictMode>
-      <Header />
-      <App authView={authView} onAuthViewChange={setAuthView}/>
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path = "/">
+            <App authView={authView} onAuthViewChange={setAuthView} />
+          </Route>
+          <Route path = "/settings">
+            <Settings />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </StrictMode>
   )
 }
